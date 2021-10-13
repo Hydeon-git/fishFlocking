442fedd33aca8a797a386b93338065facf4a9e39
Hydeon-git <61042411+Hydeon-git@users.noreply.github.com>
2021-10-13 23:33:47 +0200

    Full algorithm with problems

--- Flocking/Assets/Flock.cs
+++ Flocking/Assets/Flock.cs
@@ -2,46 +2,59 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+
 public class Flock : MonoBehaviour
 {
-    public FlockManager flockManager;    
-    float speed, globalSpeed, distance;
-    int fishNum;
+    public FlockManager flockManager;
+    int fishNum = 0;
+    float speed;
+    Vector3 direction;
 
     void Start()
     {
-        speed = Random.Range(0, 5);
-        globalSpeed = 1.0f;
+        speed = Random.Range(flockManager.minSpeed, flockManager.maxSpeed);        
     }
     void Update()
     {
-        transform.Translate(0, 0, Time.deltaTime * speed);
-        Algorithms();
+        //Rules();
+        InvokeRepeating("Rules",1 , 2);
+        transform.rotation = Quaternion.Slerp(transform.rotation,
+                             Quaternion.LookRotation(direction),
+                             flockManager.rotationSpeed * Time.deltaTime);
+        transform.Translate(0, 0, Time.deltaTime * speed);        
     }
-    void Algorithms()
+    void Rules()
     {        
-        // Flocking Rules
+        // Flocking Rules    
         Vector3 cohesion = Vector3.zero;
         Vector3 align = Vector3.zero;
         Vector3 separation = Vector3.zero;
+        
 
         foreach (GameObject go in flockManager.allFish)
         {
             if (go != this.gameObject)
             {
-                float distance = Vector3.Distance(go.transform.position,
-                                                  transform.position);
+                float distance = Vector3.Distance(go.transform.position, transform.position);
+
                 if (distance <= flockManager.neighbourDistance)
                 {
                     cohesion += go.transform.position;
+                    align += go.GetComponent<Flock>().direction;
+                    separation -= (transform.position - go.transform.position) /
+                        (distance * distance);
+                    direction = (cohesion + align + separation).normalized * speed;
                     fishNum++;
                 }
             }
         }
+        Debug.Log(fishNum);
         if (fishNum > 0)
         {
             cohesion = (cohesion / fishNum - transform.position).normalized * speed;
+            align /= fishNum;
+            speed = Mathf.Clamp(align.magnitude, flockManager.minSpeed, flockManager.maxSpeed);
         }
-            
+        
     }
 }
--- Flocking/Assets/FlockManager.cs
+++ Flocking/Assets/FlockManager.cs
@@ -7,19 +7,31 @@ public class FlockManager : MonoBehaviour
 	public GameObject fishPrefab;
 	public int numFish = 20;
 	public GameObject[] allFish;
+	public Vector3 spawnLimit;
 
+	[Range(0.0f, 5.0f)]
+	public float minSpeed;
+	[Range(0.0f, 10.0f)]
+	public float maxSpeed;
 	[Range(1.0f, 10.0f)]
 	public float neighbourDistance;
+	[Range(0.0f, 5.0f)]
+	public float rotationSpeed;
 
 	void Start()
 	{		
-		int x, y, z;
-		int dx, dy, dz;
+		spawnLimit = new Vector3(5, 5, 5);
 		allFish = new GameObject[numFish];
+
 		for (int i = 0; i < numFish; i++)
 		{
-			Vector3 pos = this.transform.position + new Vector3((x = Random.Range(-10, 10)), (y = Random.Range(-10, 10)), (z = Random.Range(-10, 10)));
-			Vector3 randomize = new Vector3((dx = Random.Range(0, 360)), (dy = Random.Range(0, 360)), (dz = Random.Range(0, 360)));
+			Vector3 pos = this.transform.position + new Vector3(
+				Random.Range(-spawnLimit.x, spawnLimit.x),
+				Random.Range(-spawnLimit.y, spawnLimit.y),
+				Random.Range(-spawnLimit.z, spawnLimit.z));
+
+
+			Vector3 randomize = new Vector3(Random.Range(-360, 360), Random.Range(-360, 360), Random.Range(-360, 360));
 			allFish[i] = (GameObject)Instantiate(fishPrefab, pos, Quaternion.LookRotation(randomize));
 			allFish[i].GetComponent<Flock>().flockManager = this;
 		}
